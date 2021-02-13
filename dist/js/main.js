34b8bfee97290b451c07897e9d2d645e0db27b8a
Liza <liza.guyvan1@gmail.com>
2021-02-13 14:32:38 +0100

    add animations,small styles fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,18 +5,53 @@ const navItemsElement = document.querySelector('.nav__items');
 const navItemsArray = document.querySelectorAll('.nav__item');
 const header = document.querySelector('header');
 
-let showMenu = false;
+const articlesArray = document.querySelectorAll('.article');
+const aboutsArray = document.querySelectorAll('div.about__item')
 
- const handleScroll = () => {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+let showMenu = false;
+const articlesPositions = [];
+const aboutsPositions = [];
+const initialize = () =>{
+    
+    articlesArray.forEach((article)=>{
+        articlesPositions.push(article.getBoundingClientRect().top - 500);
+        
+      });
+      aboutsArray.forEach((about)=>{
+        aboutsPositions.push(about.getBoundingClientRect().top - 600);
+        
+      });
+      console.log(aboutsPositions); 
+}
+const handleScroll = () => {
+    
+    if ( document.documentElement.scrollTop > 80) {
         header.classList.add('transparent');
         
       } else {
         header.classList.remove('transparent');
+      }   
+      for( let i = 0; i < articlesPositions.length; i++ ){
+        if(document.documentElement.scrollTop > articlesPositions[i] ){
+            articlesArray[i].classList.add('open');
+        } else {
+          articlesArray[i].classList.remove('open');
+        }
       }
+      for( let i = 0; i < aboutsPositions.length; i++ ){
+        if(document.documentElement.scrollTop > aboutsPositions[i] ){
+            aboutsArray[i].classList.add('open');
+        } else {
+            aboutsArray[i].classList.remove('open');
+        }
+      }
+      
+      
+       
  }
 
 const toggleMenu = () => {
+    
     console.log('toggled');
     if(!showMenu){
         hamburger.classList.add('open');
@@ -34,3 +69,4 @@ const toggleMenu = () => {
 }
 navBtn.addEventListener('click', toggleMenu);
 window.onscroll = handleScroll;
+window.onload = initialize;
